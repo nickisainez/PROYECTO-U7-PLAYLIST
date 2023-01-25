4643a9e2e52ba6b03d314ac92a64aa4cc5342ece
Daniel Moran <skaan.dmv@gmail.com>
2023-01-24 22:36:03 -0500

    Agregar puerto desde el .env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import dotenv from 'dotenv';
 import app from "./app";
+dotenv.config();
+const port = process.env.PORT;
 
-const PORT = 8000;
-
-app.listen(PORT, () => console.log(`Server init at http://localhost:${PORT}`));
+app.listen(port, () => console.log(`Server init at http://localhost:${port}`));
